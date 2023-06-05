5792b5d7bb4cd10fee8089e172ccdf6b82348af4
wonyoung2323 <72545216+wonyoung2323@users.noreply.github.com>
2023-06-05 10:29:09 +0900

    [Gold II] Title: 철로, Time: 776 ms, Memory: 140284 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.py
+++ 백준/Gold/13334. 철로/철로.py
@@ -1,30 +1,37 @@
-import sys
 import heapq as h
-input = sys.stdin.readline
+
 N = int(input())
-ele = [sorted(list(map(int, input().split())), reverse=True) for _ in range(N)]
-D = int(input())
-ele.sort()
+people = []
 
-crt_dist = 0
-crt_node = [0, D + 1]
-heap = []
-heap_in = []
+for i in range(N):
+    ele = list(map(int, input().split()))
+    ele.sort()
+    people.append(ele)
+L = int(input())
+
+people.sort(key=lambda x : (x[1], x[0]))
+
+end = people[0][1]
+start = end - L
+hq = []
 answer = 0
-for i in ele:
-    h.heappush(heap, i)
-
-while heap:
-    crt_node = h.heappop(heap)
-    if abs(crt_node[0] - crt_node[1]) > D:
-        continue
-    crt_dist = crt_node[0]
-    if crt_dist - D <= crt_node[1]:
-        h.heappush(heap_in, (crt_node[1], crt_node[0]))
-        if crt_dist - D > heap_in[0][0]:
-            while heap_in and heap_in[0][0] < crt_dist - D:
-                h.heappop(heap_in)
-        if answer < len(heap_in):
-            answer = len(heap_in)
+count = 0
+for i in people:
+    if i[1] - i[0] > L:
+            continue
+    if i[0] >= start and i[1] <= end:
+        count += 1
+        answer = max(answer, count)
+        h.heappush(hq, i)
+    elif i[1] > end:
+        end = i[1]
+        start = end - L
+        count += 1
+        h.heappush(hq, i)
+        while hq and hq[0][0] < start:
+            h.heappop(hq)
+            count -= 1
+        answer = max(answer, count)
+        
 
 print(answer)
